aa0ef847503b8bc3ecb3c5c5a8738c2ca08d4ac6
AvoFlex <rosinaaa@gmail.com>
2024-11-09 21:04:19 +0000

    Revert "nextjs and shadcn initial setup"

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Gastos del mes: Octubre 2024</h1>
-    </header>
-  );
-}
