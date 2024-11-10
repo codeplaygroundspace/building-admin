f39ec827e7f6355ee217d33d88020e5b0242a005
codeplaygroundspace <rosinaaa@gmail.com>
2024-11-10 19:01:53 +0100

    fix typo and delete unused info file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Header from "../components/Header";
-import PaymentsInfo from "@/components/PaymentsInfo";
-import ContactInfo from "@/components/ContactInfo";
-import InfoDebt from "@/components/InfoDebt";
-import FundSummary from "@/components/FundSummary";
-import ExpenseBreakdown from "@/components/ExpenseBreakdown";
-import CreditsDebitsBreakdown from "@/components/CreditsDebitsBreakdown";
-import ApartmentFinancialBreakdown from "@/components/ApartmentFinancialBreakdown";
+import PaymentsInfo from "@/components/InfoPayments";
+import ContactInfo from "@/components/InfoContacts";
+import InfoDebt from "@/components/FinDebt";
+import FundSummary from "@/components/FinFundSummary";
+import ExpenseBreakdown from "@/components/FinExpenseBreakdown";
+import CreditsDebitsBreakdown from "@/components/FinCreditsDebitsBreakdown";
+import ApartmentFinancialBreakdown from "@/components/FinApartmentFinancialBreakdown";
 
 export default function Home() {
   return (
