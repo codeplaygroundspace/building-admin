2913d5e6310ad63245477d16c40ccd2991b9059c
codeplaygroundspace <rosinaaa@gmail.com>
2024-11-29 11:41:54 +0000

    add expenses avatar

---
+++ src/components/ExpenseListItem.tsx
@@ -0,0 +1,36 @@
+import { formatCurrency } from "../utils/formatCurrency";
+
+interface ExpenseListItemProps {
+  category_name: string;
+  description: string;
+  amount: number;
+  colour: string;
+}
+
+export default function ExpenseListItem({
+  category_name,
+  description,
+  amount,
+  colour,
+}: ExpenseListItemProps) {
+  return (
+    <li className="flex justify-between items-start">
+      <div className="flex items-start gap-2 pr-4">
+        <div
+          className="size-12 aspect-square rounded-lg border-2 border-gray-300 flex items-center justify-center bg-gray-100"
+          style={{ backgroundColor: colour || "black" }}
+          aria-hidden="true"
+        >
+          <span className="text-white text-xl font-bold">
+            {category_name.charAt(0)}
+          </span>
+        </div>
+        <div>
+          <h3 className="text-lg font-semibold">{category_name}</h3>
+          <p className="text-neutral-500">{description}</p>
+        </div>
+      </div>
+      <p className="whitespace-nowrap">{formatCurrency(amount)}</p>
+    </li>
+  );
+}
