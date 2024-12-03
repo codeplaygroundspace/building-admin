adb74061cfbe5732f131da642e85b34ade11590f
codeplaygroundspace <rosinaaa@gmail.com>
2024-12-03 17:45:32 +0000

    add fetch data

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,11 +1,9 @@
 import PaymentsInfo from "@/components/InfoPayments";
 import ContactInfo from "@/components/InfoContacts";
-import MainMenu from "@/components/MainMenu";
 
 export default function Info() {
   return (
     <>
-      <MainMenu />
       <PaymentsInfo />
       <ContactInfo />
     </>
